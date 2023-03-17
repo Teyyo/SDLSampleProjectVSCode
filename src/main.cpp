53c8c542544c984a8bd8d60abdd84eda90ecc698
Teyyo <64683483+Teyyo@users.noreply.github.com>
2023-03-17 18:48:45 +0100

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+int main(int argc, char* args[]) 
+{
+
+	std::cout << "SDL Sample Project (for VS CODE) by Laci!\n\nIs working!" << std::endl;
+
+	return 0;
+}
